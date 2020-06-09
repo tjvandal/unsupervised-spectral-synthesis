713b00fc745b1a02bdeebc1ebd3cda9fa4728063
tjvandal <tvandal20@gmail.com>
2020-06-09 15:10:58 -0700

    organizing test set

--- scripts/test_set.py
+++ scripts/test_set.py
@@ -6,19 +6,9 @@ import numpy as np
 import xarray as xr
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import geonexl1b
+from data import geonexl1b
 
-path1 = '/nex/projects/goesscratch/weile/AHI05_java/output/HDF4'
-#G16_path = '/nex/datapool/geonex/public/GOES16/GEONEX-L1B/'
-path2 = '/nex/datapool/geonex/public/GOES17/GEONEX-L1B/'
-sensor1 = 'H8'
-sensor2 = 'G17'
 
-data_path = 'data/Test/'
-
-year = 2019
-dayofyear = 2
-hour = 4
 
 def test_set_pair(path1, path2, sensor1, sensor2, year, dayofyear,
                  hour=20, minute=0):
@@ -38,9 +28,46 @@ def copy_file(file, dest):
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
     shutil.copy(file, dest)
+    
+def sensor_files(path, sensor, year, dayofyear, hour=20, minute=0):
+    geo = geonexl1b.GeoNEXL1b(path, sensor)
+    files = geo.files(year=year, dayofyear=dayofyear)
+    if len(files) == 0:
+        return None
+    files = files[files['hour'] == hour]
+    files = files[files['minute'] == minute]
+    return files
+    
+    
+def paired_test_set():
+    path1 = '/nex/projects/goesscratch/weile/AHI05_java/output/HDF4'
+    #G16_path = '/nex/datapool/geonex/public/GOES16/GEONEX-L1B/'
+    path2 = '/nex/datapool/geonex/public/GOES17/GEONEX-L1B/'
+    sensor1 = 'H8'
+    sensor2 = 'G17'
 
+    data_path = 'data/Test/'
 
-files = test_set_pair(path1, path2, sensor1, sensor2, year, dayofyear, hour=hour)
-for i, row in files.iterrows():
-    copy_file(row['file1'], data_path)
-    copy_file(row['file2'], data_path)
+    year = 2019
+    dayofyear = 2
+    hour = 4
+    files = test_set_pair(path1, path2, sensor1, sensor2, year, dayofyear, hour=hour)
+    for i, row in files.iterrows():
+        copy_file(row['file1'], data_path)
+        copy_file(row['file2'], data_path)
+        
+def single_test_set():
+    #path = '/nex/datapool/geonex/public/GOES17/GEONEX-L1G/'
+    path = '/nex/projects/goesscratch/weile/AHI05_java/output/HDF4'
+    sensor = 'H8'
+    year = 2019
+    dayofyear = 2
+    hour = 4
+    data_path = '/nobackupp10/tvandal/nex-ai-geo-translation/data/Test/'
+    files = sensor_files(path, sensor, year, dayofyear, hour)
+    for i, row in files.iterrows():
+        print(f"Copying file {row['file']} to {data_path}")
+        copy_file(row['file'], data_path)
+    
+if __name__ == '__main__':
+    single_test_set()
